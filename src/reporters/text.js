4d6b343438d0130486da152a2581601e2ddf12b7
Josh Graber <josh.graber.85@gmail.com>
2017-12-06 07:38:53 -0500

    Add initial diagnostic reporter
    Eventually we would like the reporter to be more vague, giving hints about current pet state instead of dumping all pet properties.

--- src/reporters/text.js
+++ src/reporters/text.js
@@ -4,6 +4,12 @@ import io from '../io'
 export function report(pet, day) {
   console.log(`***** DAY ${day} *****`)
   console.log(`${pet.name} wakes up`)
+  console.log(`pet.happiness = ${pet.happiness}`)
+  console.log(`pet.food = ${pet.food}`)
+  console.log(`pet.energy = ${pet.energy}`)
+  console.log(`pet.health = ${pet.health}`)
+  console.log(`pet.weight = ${pet.weight}`)
+  console.log(`pet.experience = ${pet.experience}`)
 }
 
 export async function selectAction(actions) {
